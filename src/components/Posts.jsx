c09877aa26b5e4346080c8eb3a356ba6dee44cd8
waterbeen0530 <hange253@gmail.com>
2022-09-14 14:36:39 +0900

    [CREATE] pagination 구현

---
+++ src/components/Posts.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import Pagination from "./Pagination";
+
+export default function Posts() {
+  const [posts, setPosts] = useState([]);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+  const offset = (page - 1) * limit;
+
+  const getItems = async () => {
+    await axios
+      .get(`https://jsonplaceholder.typicode.com/posts`)
+      .then((res) => {
+        const data = res.data;
+        setPosts(data);
+      });
+  };
+  useEffect(() => {
+    getItems();
+  }, []);
+
+  return (
+    <>
+      <Container>
+        <header>
+          <h1>게시물 목록</h1>
+        </header>
+
+        <label>
+          페이지 당 표시할 게시물 수:&nbsp;
+          <select
+            type="number"
+            value={limit}
+            onChange={({ target: { value } }) => setLimit(Number(value))}
+          >
+            <option value="10">10</option>
+            <option value="12">12</option>
+            <option value="20">20</option>
+            <option value="50">50</option>
+            <option value="100">100</option>
+          </select>
+        </label>
+
+        <main>
+          {posts.slice(offset, offset + limit).map(({ id, title, body }) => (
+            <article key={id}>
+              <h3>
+                {id}. {title}
+              </h3>
+              <p>{body}</p>
+            </article>
+          ))}
+        </main>
+
+        <footer>
+          <Pagination
+            total={posts.length}
+            limit={limit}
+            page={page}
+            setPage={setPage}
+          />
+        </footer>
+      </Container>
+    </>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 800px;
+  margin: 0 auto;
+`;
